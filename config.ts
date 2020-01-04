3124f0b4f36d324cce89ff8a0e08608abb399e55
Dariusz Niemczyk <3636685+Palid@users.noreply.github.com>
2020-01-04 01:42:46 +0100

    Update config and package.json

--- config.ts
+++ config.ts
@@ -1,7 +1,11 @@
 export const CONFIG = {
-  domain:
-    process.env.NODE_ENV === "development"
+  get domain() {
+    return process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
-      : "https://c3sessions.bgp.wtf/",
-  resource: "schedule"
+      : `${window.location.origin}`;
+  },
+
+  get resource() {
+    return "schedule";
+  }
 };
